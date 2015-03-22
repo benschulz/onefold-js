328c3c7a85fb8879d4c3da05d7bd28b0e335b2f3
benshu <benshu@benshu.de>
2015-03-22 23:12:07 +0100

    Constant functions (true, false, nop, null zero) are deprecated.

--- src/functions.js
+++ src/functions.js
@@ -4,6 +4,7 @@ define([], function () {
     var constant = x => () => x;
 
     return {
+        // TODO with arrow functions these can go away
         true: constant(true),
         false: constant(false),
         nop: constant(undefined),
