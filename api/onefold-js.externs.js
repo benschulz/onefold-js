050ae19e36bae0720a82aa1b95226047c9e48996
benshu <benshu@benshu.de>
2015-03-30 07:33:36 +0200

    Moved APIs into the `de.benshu` namespace.

--- api/onefold-js.externs.js
+++ api/onefold-js.externs.js
@@ -1,52 +1,46 @@
-/**
- * @namespace
- * @suppress {duplicate}
- */
-var onefold = onefold || {};
+/** @namespace */
+de.benshu.onefold.js = {};
 
 /** @namespace */
-onefold.js = {};
-
-/** @namespace */
-onefold.js.objects = {};
+de.benshu.onefold.js.objects = {};
 
 /**
  * @param {*} a
  * @param {*} b
  * @returns {boolean}
  */
-onefold.js.objects.areEqual = function (a, b) { };
+de.benshu.onefold.js.objects.areEqual = function (a, b) { };
 
 /**
  * @param {Object} object
  * @param {...Object} extensions
  * @returns {Object}
  */
-onefold.js.objects.extend = function (object, extensions) {};
+de.benshu.onefold.js.objects.extend = function (object, extensions) {};
 
 /**
  * @param {Object} owner
  * @param {function(string, *)} action
  * @returns {undefined}
  */
-onefold.js.objects.forEachProperty = function (owner, action) {};
+de.benshu.onefold.js.objects.forEachProperty = function (owner, action) {};
 
 /**
  * @param {Object} owner
  * @param {string} propertyName
  * @returns {boolean}
  */
-onefold.js.objects.hasOwn = function (owner, propertyName) {};
+de.benshu.onefold.js.objects.hasOwn = function (owner, propertyName) {};
 
 /**
  * @param {Object} source
  * @param {function(*, string, *):*} mapper
  * @returns {Object}
  */
-onefold.js.objects.mapProperties = function (source, mapper) {};
+de.benshu.onefold.js.objects.mapProperties = function (source, mapper) {};
 
 /** @namespace */
-onefold.js.arrays = {};
+de.benshu.onefold.js.arrays = {};
 
 /**
  * @template E
@@ -54,14 +48,14 @@ onefold.js.arrays = {};
  * @param {E} value
  * @returns {boolean}
  */
-onefold.js.arrays.contains = function (array, value) {};
+de.benshu.onefold.js.arrays.contains = function (array, value) {};
 
 /**
  * @template E
  * @param {Array<E>} array
  * @returns {Array<E>}
  */
-onefold.js.arrays.distinct = function (array) {};
+de.benshu.onefold.js.arrays.distinct = function (array) {};
 
 /**
  * @template E, I
@@ -69,7 +63,7 @@ onefold.js.arrays.distinct = function (array) {};
  * @param {function(E):Array<I>} mapper
  * @returns {Array<I>}
  */
-onefold.js.arrays.flatMap = function (array, mapper) {};
+de.benshu.onefold.js.arrays.flatMap = function (array, mapper) {};
 
 /**
  * @template E
@@ -77,7 +71,7 @@ onefold.js.arrays.flatMap = function (array, mapper) {};
  * @param {function(E):boolean} predicate
  * @returns {E}
  */
-onefold.js.arrays.single = function (array, predicate) {};
+de.benshu.onefold.js.arrays.single = function (array, predicate) {};
 
 /**
  * @template E
@@ -85,7 +79,7 @@ onefold.js.arrays.single = function (array, predicate) {};
  * @param {function(E):boolean} predicate
  * @returns {?E}
  */
-onefold.js.arrays.singleOrNull= function (array, predicate) {};
+de.benshu.onefold.js.arrays.singleOrNull= function (array, predicate) {};
 
 /**
  * @template E
@@ -93,43 +87,43 @@ onefold.js.arrays.singleOrNull= function (array, predicate) {};
  * @param {function(E, E):number} comparator
  * @returns {Array<E>}
  */
-onefold.js.arrays.stableSort = function (array, comparator) {};
+de.benshu.onefold.js.arrays.stableSort = function (array, comparator) {};
 
 /** @namespace */
-onefold.js.functions = {};
+de.benshu.onefold.js.functions = {};
 
 /**
  * @template T
  * @param {T} value
  * @returns {function():T}
  */
-onefold.js.functions.constant = function (value) { };
+de.benshu.onefold.js.functions.constant = function (value) { };
 
 /**
  * @template T
  * @param {T} value
  * @returns {T}
  */
-onefold.js.functions.identity = function (value) { };
+de.benshu.onefold.js.functions.identity = function (value) { };
 
 /** @namespace */
-onefold.js.strings = {};
+de.benshu.onefold.js.strings = {};
 
 /**
  * @param {string} camelCased
  * @returns {string}
  */
-onefold.js.strings.convertCamelToHyphenCase = function (camelCased) {};
+de.benshu.onefold.js.strings.convertCamelToHyphenCase = function (camelCased) {};
 
 /**
  * @param {string} hyphenCased
  * @returns {string}
  */
-onefold.js.strings.convertHyphenToCamelCase = function (hyphenCased) {};
+de.benshu.onefold.js.strings.convertHyphenToCamelCase = function (hyphenCased) {};
 
 /**
  * @param {string} formatString
  * @param {...*} args
  * @returns {string}
  */
-onefold.js.strings.format = function (formatString, args) {};
+de.benshu.onefold.js.strings.format = function (formatString, args) {};
